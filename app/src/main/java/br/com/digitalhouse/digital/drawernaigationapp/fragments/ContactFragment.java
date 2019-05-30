c42d50d35db13fccacc5cde3c3def4291fa5d001
kelydsc <kelydsc@yahoo.com.br>
2019-05-29 22:18:27 -0300

    aula 26 - card view e menu hamburgue

---
+++ app/src/main/java/br/com/digitalhouse/digital/drawernaigationapp/fragments/ContactFragment.java
@@ -0,0 +1,49 @@
+package br.com.digitalhouse.digital.drawernaigationapp.fragments;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import br.com.digitalhouse.digital.drawernaigationapp.MainActivity;
+import br.com.digitalhouse.digital.drawernaigationapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ContactFragment extends Fragment {
+
+
+    public ContactFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_contact, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
+        super.onViewCreated(view, savedInstanceState);
+
+        Button btnGotoRegister = view.findViewById(R.id.btnRegister);
+        btnGotoRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (getActivity() instanceof MainActivity) {
+                    ((MainActivity) getActivity()).replaceFragment(new RegisterFragment());
+                }
+            }
+        });
+    }
+}
